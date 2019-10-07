e61784a72b23de1c0b525458ab574439ea064b0a
emma <efwarkentin@gmail.com>
2019-10-07 08:58:31 -0600

    Created an initial main menu

---
+++ Strange Brews/Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // for the changing of the scenes
+
+public class MainMenu : MonoBehaviour
+{
+    // To change the scene when the play button is clicked
+    // make sure that this is called when the play button is clicked
+    public void PlayGame()
+    {
+        // this just moves it to the next scene in the queue
+        // add to the queue in file/build settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    // To quit the game
+    // Hook this up to the quit button
+    public void QuitGame()
+    {
+        // this won't quit in Unity so this is to know it happened
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+
