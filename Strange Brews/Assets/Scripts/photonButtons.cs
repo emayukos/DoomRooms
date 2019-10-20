32d40c2abc49f8e74e876843e8bdc793cb0cf4ff
Emi <ems032@mail.usask.ca>
2019-10-20 11:04:27 -0600

    cleaned up files and implemented basic GUI for connecting to photon and joining room. still need to get message to display on press of join room button.

---
+++ Strange Brews/Assets/Scripts/photonButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class photonButtons : MonoBehaviour
+{
+	public menuLogic mLogic;
+
+	public InputField createRoomInput, joinRoomInput;
+	
+	public void onClickCreateRoom()
+	{
+		if(createRoomInput.text.Length >= 1)
+			_ = PhotonNetwork.CreateRoom(createRoomInput.text, new RoomOptions() { MaxPlayers = 2 }, null);
+			
+	}
+	
+	public void onClickJoinRoom()
+	{
+		PhotonNetwork.JoinRoom(joinRoomInput.text);
+	}
+	
+	// not working?
+	private void OnJoinedRoom() {
+		mLogic.disableMenuUI();
+		Debug.Log("We are connected to the room!");
+	}
+	
+	
+}
