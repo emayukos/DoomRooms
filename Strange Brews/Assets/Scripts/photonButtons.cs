9ccc99075845d7cd29a1f5a14b58e2aff9faeecf
Emi <ems032@mail.usask.ca>
2019-10-21 14:09:31 -0600

    added revisions to GUI for connecting to room

--- Strange Brews/Assets/Scripts/photonButtons.cs
+++ Strange Brews/Assets/Scripts/photonButtons.cs
@@ -8,20 +8,22 @@ public class photonButtons : MonoBehaviour
 	public menuLogic mLogic;
 
 	public InputField createRoomInput, joinRoomInput;
-	
+
+	[System.Obsolete]
 	public void onClickCreateRoom()
 	{
-		if(createRoomInput.text.Length >= 1)
+		if(createRoomInput.text.Length >= 1) // checks if we typed anything in out input
 			_ = PhotonNetwork.CreateRoom(createRoomInput.text, new RoomOptions() { MaxPlayers = 2 }, null);
-			
+				Debug.Log("room created!");
 	}
 	
 	public void onClickJoinRoom()
 	{
 		PhotonNetwork.JoinRoom(joinRoomInput.text);
 	}
+
 	
-	// not working?
+	[System.Obsolete]
 	private void OnJoinedRoom() {
 		mLogic.disableMenuUI();
 		Debug.Log("We are connected to the room!");
