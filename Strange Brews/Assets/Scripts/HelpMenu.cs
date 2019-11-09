b42c9dee7b784eab2f8e75af7df0f45f6fad2932
Chandra Lyons <cml422@usask.ca>
2019-11-09 14:26:48 -0600

    - added help menu turned on/off by 'h' key

---
+++ Strange Brews/Assets/Scripts/HelpMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpMenu : MonoBehaviour
+{
+    int onOff;
+    GameObject child;
+
+    void Awake()
+    {
+        onOff = 1;
+    }
+
+    private void Start()
+    {
+        child = GameObject.Find("Help Menu Panel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            onOff = onOff * -1;
+        }
+
+        if(onOff < 0)
+        {
+            //turn off help menu
+            child.SetActive(false);
+        }
+        if(onOff > 0)
+        {
+            //turn on help menu
+            child.SetActive(true);
+        }
+    }
+
+}
