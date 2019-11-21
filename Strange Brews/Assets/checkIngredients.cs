811b27c530297ffcd685ed0f29246852948f34f7
emma <efwarkentin@gmail.com>
2019-11-21 12:39:17 -0600

    Added the mini menu volume settings

---
+++ Strange Brews/Assets/checkIngredients.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkIngredients : MonoBehaviour
+{
+    Inventory inventory;
+    bool found1, found2, found3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inventory.searchItem("ingredients1");
+        
+    }
+
+
+
+
+}
