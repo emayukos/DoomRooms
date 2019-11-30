48bc093a3095ff77d4a220e2885d223f1e1b9624
Emi <ems032@mail.usask.ca>
2019-11-30 14:08:14 -0600

    got multiplayer working when pressing play at different times

---
+++ Strange Brews/Assets/Scripts/ShowPlayerInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShowPlayerInfo : Photon.MonoBehaviour
+
+{
+//    private Canvas textGo;
+//    private TextMesh tm;
+//    public bool DisableOnOwnObjects;
+
+//    void Start()
+//    {
+//        if (tm == null) //wenn textmesh
+//        {	
+//            textGo =  GetComponentInChildren<Canvas>();
+//			tm = textGo.GetComponent<TextMesh>;
+            
+//        }
+//    }
+
+//    void Update()
+//    {
+//        bool showInfo = !this.DisableOnOwnObjects || this.photonView.isMine;
+//        if (tm != null)
+//        {
+//            tm.SetActive(showInfo);
+//        }
+//        if (!showInfo)
+//        {
+//            return;
+//        }
+//        PhotonPlayer owner = this.photonView.owner;
+//        if (owner != null)
+//        {
+//            tm.text = (string.IsNullOrEmpty(owner.NickName)) ? "player" + owner.ID : owner.NickName;
+//        }
+//        else if (this.photonView.isSceneView)
+//        {
+//            tm.text = "scn";
+//        }
+//        else
+//        {
+//            tm.text = "n/a";
+//        }
+//    }
+}
